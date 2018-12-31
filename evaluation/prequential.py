f7c9fc3a7a7005ae3e65257733290bf8b585adc5
feranor <36387607+ellatran@users.noreply.github.com>
2018-12-31 17:46:50 +0100

    Comments added

--- evaluation/prequential.py
+++ evaluation/prequential.py
@@ -7,6 +7,8 @@ def prequential(X, y, clf, n_train=1):
     """Prequential Evaluation: instances are first used to test, and then to train
     :return the label predictions for each test instance, and the associated running time
     """
+    #Prequential evaliuation is used in SAMOA. More infot can be gathered in the link below
+    #https://samoa.incubator.apache.org/documentation/Prequential-Evaluation-Task.html
     row_num = y.shape[0]
     # Split an init batch
     X_init = X[0:n_train]
@@ -19,8 +21,10 @@ def prequential(X, y, clf, n_train=1):
     y_pre = np.zeros(row_num - n_train)
     time = np.zeros(row_num - n_train)
 
+    #initiate model with the data defined by the number of n_train.
     clf.fit(X_init, y_init)
 
+    #first predict the instannce and store in in an array then partial_fit it to clf.
     for i in range(0, row_num - n_train):
         start_time = clock()
         y_pre[i] = clf.predict(X_train[i, :].reshape(1, -1))
